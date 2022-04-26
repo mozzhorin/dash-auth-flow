62ff4f16711421080702486d44936324b30e7c87
Iurii Mozzhorin <mozzhorin@posteo.de>
2022-04-26 13:23:18 +0200

    ignore

--- dash/utilities/config.py
+++ dash/utilities/config.py
@@ -1,7 +1,8 @@
 import configparser
 from sqlalchemy import create_engine
 
-config = configparser.ConfigParser()
-config.read("/app/dash/utilities/config.txt")
+#config = configparser.ConfigParser()
+#config.read("/app/dash/utilities/config.txt")
 
-engine = create_engine(config.get('database', 'con'))
+#engine = create_engine(config.get('database', 'con'))
+engine = create_engine('sqlite:///users.db')
